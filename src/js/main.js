f4d036fe5fcab8c8fa87818012f0fddd1a132fe7
Stanislaw Mikolajczyk <mikolajczykstanislaw415@gmail.com>
2024-03-25 17:57:09 +0100

    make page responsive

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,14 @@
 window.addEventListener('scroll', () => {
 	const header = document.querySelector('header');
+	const profilePictureBlack = document.querySelector('.profile-icon-black');
+	const profilePictureWhite = document.querySelector('.profile-icon-white');
 	if (scrollY > 10) {
 		header.classList.add('white-header');
+		profilePictureBlack.style.display = "block"
+		profilePictureWhite.style.display = "none"
 	} else {
 		header.classList.remove('white-header');
+		profilePictureBlack.style.display = "none"
+		profilePictureWhite.style.display = "block"
 	}
 });
